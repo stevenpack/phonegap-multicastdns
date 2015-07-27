9a2536dc04ca230563d7e47ab98797f1c85bffea
stevenpack <steven_john_pack@yahoo.com.au>
2015-07-27 16:49:27 +1000

    verbose logging for plugin. was getting no response and no logs in logcat after first integration

--- src/android/MulticastDNSPlugin.java
+++ src/android/MulticastDNSPlugin.java
@@ -11,29 +11,40 @@ public class MulticastDNSPlugin extends CordovaPlugin {
 
 	@Override
 	public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-		if (action.equals("query")) {
-			final Context context = this.cordova.getActivity().getApplicationContext();
-			final String host = data.getString(0);
-			final String multicastIP = data.getString(1);
-            final int port = data.getInt(2);
-			Log.d(TAG, String.format("Will query for %s on %s:%s", host, multicastIP, port));
-			final CallbackContext cb = callbackContext;
-			cordova.getThreadPool().execute(new Runnable() {
-				public void run() {
-					try {
-						MulticastDnsRequestor r = new MulticastDnsRequestor(multicastIP, port, context);
-						String answer = r.query(host);
-						cb.success(answer); // Thread-safe.
-					} catch (Exception ex) {
-						Log.e(TAG, "Failed to lookup host. " + ex.getStackTrace());
-						cb.error(ex.getMessage());
+		try {
+			Log.i(TAG, "Entered plugin execute. Action is: " + action);
+			if (action.equals("query")) {
+				Log.v(TAG, "Getting context...");
+				final Context context = this.cordova.getActivity().getApplicationContext();
+				Log.v(TAG, "Getting host...");
+				final String host = data.getString(0);
+				Log.v(TAG, "Getting multicastIP...");
+				final String multicastIP = data.getString(1);
+				Log.v(TAG, "Getting port...");
+				final int port = data.getInt(2);
+				Log.i(TAG, String.format("Will query for %s on %s:%s", host, multicastIP, port));
+				final CallbackContext cb = callbackContext;
+				Log.i(TAG, "Starting new thread for request");
+				cordova.getThreadPool().execute(new Runnable() {
+					public void run() {
+						try {
+							Log.i(TAG, "New thread for request");
+							MulticastDnsRequestor r = new MulticastDnsRequestor(multicastIP, port, context);
+							String answer = r.query(host);
+							cb.success(answer); // Thread-safe.
+						} catch (Exception ex) {
+							Log.e(TAG, "Failed to lookup host. " + ex.getStackTrace());
+							cb.error(ex.getMessage());
+						}
 					}
-				}
-			});
-			return true;
-		} else {
-			callbackContext.error("unknown action: " + action);
+				});
+				return true;
+			} else {
+				callbackContext.error("unknown action: " + action);
+				return false;
+			}
+		} catch (Exception ex) {
+			callbackContext.error("Failed to execute plugin: " + ex.getMessage());
 			return false;
 		}
 	}
